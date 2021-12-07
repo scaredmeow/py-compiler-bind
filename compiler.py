a2088d995f5365a118e661a4193846caf4fbb27f
Neil Riego <neilchristianriego3@gmail.com>
2021-12-08 00:02:53 +0800

    First Commit

--- compiler.py
+++ compiler.py
@@ -1,18 +1,18 @@
 import lex_analyzer as lex
 import sys
 
-# print("Enter Expression: ")
-# expression = sys.stdin.read()
+print("Enter Expression: ")
+expression = sys.stdin.read()
 
 print("LEXEME\t\t\t\tTOKEN")
 
 errors = []
 
-expression = '''
-int a,b
-shoot('as)
-as
-'''
+# expression = '''
+# int a,b
+# shoot('as)
+# as
+# '''
 
 for token in lex.tokenize(expression):
   print(str(token.value)+"\t\t"+str(token.type))
