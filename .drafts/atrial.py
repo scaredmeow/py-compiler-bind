8bd24d96bc77112615325da2d1e120e74dd0b408
Neil Riego <neilchristianriego3@gmail.com>
2021-12-08 00:01:21 +0800

    first commit

---
+++ .drafts/atrial.py
@@ -0,0 +1,7 @@
+import re
+
+txt = '\"abcsadasjhdsajhdadakldkl;\"'
+
+name = txt[1:len(txt)-1]
+
+print(name)
---
+++ compiler.py
@@ -0,0 +1,24 @@
+import lex_analyzer as lex
+import sys
+
+# print("Enter Expression: ")
+# expression = sys.stdin.read()
+
+print("LEXEME\t\t\t\tTOKEN")
+
+errors = []
+
+expression = '''
+int a,b
+shoot('as)
+as
+'''
+
+for token in lex.tokenize(expression):
+  print(str(token.value)+"\t\t"+str(token.type))
+
+errors = token.errors
+
+for i in errors:
+  print(i)
+
