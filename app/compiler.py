ab7afd4375baa0c7ab29af91650fd2138076d888
Neil Riego <neilchristianriego3@gmail.com>
2021-12-22 19:03:31 +0800

    Update Format

--- app/compiler.py
+++ app/compiler.py
@@ -1,149 +1,162 @@
-import lex, os
-deli_reserved = {"sep(:)":["base"],"open_array":["arr"],"array_delim":["]"]}
-#----------------------------------------------------------------------------------------------------------
+import lex
+import os
+deli_reserved = {"sep(:)": ["base"], "open_array": [
+    "arr"], "array_delim": ["]"]}
+# ----------------------------------------------------------------------------------------------------------
 # os.chdir('app')                                                           # Getting tokens
 with open("tokens/rword.txt") as f:
-  rwords = f.read()
-  rwords= list(item for item in rwords.split('\n') if item.strip())
+    rwords = f.read()
+    rwords = list(item for item in rwords.split('\n') if item.strip())
 with open("tokens/rsymbol.txt") as f:
-  rsymbol = f.read()
-  rsymbol = list(item for item in rsymbol.split('\n') if item.strip())  
+    rsymbol = f.read()
+    rsymbol = list(item for item in rsymbol.split('\n') if item.strip())
 with open("tokens/rdef/whitespace.txt") as f:
-  whitespace = f.read()
-  whitespace = list(item for item in whitespace.split('\n') if item.strip())
+    whitespace = f.read()
+    whitespace = list(item for item in whitespace.split('\n') if item.strip())
 with open("tokens/rdef/open_func.txt") as f:
-  open_func = f.read()
-  open_func = list(item for item in open_func.split('\n') if item.strip())
+    open_func = f.read()
+    open_func = list(item for item in open_func.split('\n') if item.strip())
 with open("tokens/rdef/open_codeBlock.txt") as f:
-  open_codeBlock = f.read()
-  open_codeBlock = list(item for item in open_codeBlock.split('\n') if item.strip())
+    open_codeBlock = f.read()
+    open_codeBlock = list(
+        item for item in open_codeBlock.split('\n') if item.strip())
 with open("tokens/rdef/line_delim.txt") as f:
-  line_delim = f.read()
-  line_delim = list(item for item in line_delim.split('\n') if item.strip())
+    line_delim = f.read()
+    line_delim = list(item for item in line_delim.split('\n') if item.strip())
 with open("tokens/rdef/expr_delim.txt") as f:
-  expr_delim = f.read()
-  expr_delim = list(item for item in expr_delim.split('\n') if item.strip())
+    expr_delim = f.read()
+    expr_delim = list(item for item in expr_delim.split('\n') if item.strip())
 with open("tokens/rdef/id_rdef.txt") as f:
-  id_rdef = f.read()
-  id_rdef = list(item for item in id_rdef.split('\n') if item.strip())
+    id_rdef = f.read()
+    id_rdef = list(item for item in id_rdef.split('\n') if item.strip())
 with open("tokens/rdef/alphanumeric.txt") as f:
-  alphanumeric = f.read()
-  alphanumeric = list(item for item in alphanumeric.split('\n') if item.strip())
+    alphanumeric = f.read()
+    alphanumeric = list(
+        item for item in alphanumeric.split('\n') if item.strip())
 deli_reserved["whitespace"] = whitespace
 deli_reserved["open_func"] = open_func
 deli_reserved["open_codeBlock"] = open_codeBlock
 deli_reserved["line_delim"] = line_delim
 deli_reserved["expr_delim"] = expr_delim
 deli_reserved["id_rdef"] = id_rdef
-#----------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------
+
 
 class lexer:
-  def __init__(self,expression):
-    tmptype = []
-    tmpvalue = []
-    tmpline = []
-    tmpcolumn = []
-    tmperror = []
+    def __init__(self, expression):
+        tmptype = []
+        tmpvalue = []
+        tmpline = []
+        tmpcolumn = []
+        tmperror = []
 
-    for token in lex.tokenize(expression):
-      tmptype.append(token.type)
-      tmpvalue.append(token.value)
-      tmpline.append(token.line)
-      tmpcolumn.append(token.column)
-      tmperror.append(token.error)      
+        for token in lex.tokenize(expression):
+            tmptype.append(token.type)
+            tmpvalue.append(token.value)
+            tmpline.append(token.line)
+            tmpcolumn.append(token.column)
+            tmperror.append(token.error)
 
-    rdefinition = {"whitespace":[" ","\t"],"open_codeBlock":["{"],"open_array":["["],"open_func":["("],
-                   "close_block":[")","]","}"],"sep(:)":[":"],"line_delim":["\n"],"arith_op":["+","-","*","/","%","!"],
-                   "rela_op":[">","<","==","=!","=>","=>","=<"],"comb_op":["=-","=*","=/","=%","=+"],
-                   "logi_op":["and","or","n"],"una_op":["++","--"],"separator":[",","&"]}
-    rdefinition["whitespace"] += rdefinition["line_delim"]
-    rdefinition["array_delim"] = rdefinition["comb_op"] + rdefinition["open_array"] + rdefinition["close_block"] + rdefinition["whitespace"]
-    rdefinition["id_rdef"] = alphanumeric + rdefinition["whitespace"] + ["_","\""] + rdefinition["close_block"]
-    rdefinition["expr_delim"] = rdefinition["una_op"] + rdefinition["id_rdef"] + ["!",","]
-    rdefinition["operator"] = rdefinition["arith_op"] + rdefinition["rela_op"] + rdefinition["comb_op"] + rdefinition["una_op"]
-    rdefinition["num_delim"] = (rdefinition["whitespace"] + rdefinition["open_func"] + rdefinition["operator"] 
-                                + rdefinition["close_block"] +  rdefinition["separator"] + rdefinition["line_delim"])
-    rdefinition["id_delim"] = rdefinition["num_delim"] + rdefinition["open_array"]
-    rdefinition["open_codeBlock"]+=rdefinition["whitespace"]
-    rdefinition["open_array"]+=rdefinition["whitespace"]
-    rdefinition["open_func"]+=rdefinition["whitespace"]
+        rdefinition = {"whitespace": [" ", "\t"], "open_codeBlock": ["{"], "open_array": ["["], "open_func": ["("],
+                       "close_block": [")", "]", "}"], "sep(:)": [":"], "line_delim": ["\n"], "arith_op": ["+", "-", "*", "/", "%", "!"],
+                       "rela_op": [">", "<", "==", "=!", "=>", "=>", "=<"], "comb_op": ["=-", "=*", "=/", "=%", "=+"],
+                       "logi_op": ["and", "or", "n"], "una_op": ["++", "--"], "separator": [",", "&"]}
+        rdefinition["whitespace"] += rdefinition["line_delim"]
+        rdefinition["array_delim"] = rdefinition["comb_op"] + rdefinition["open_array"] + \
+            rdefinition["close_block"] + rdefinition["whitespace"]
+        rdefinition["id_rdef"] = alphanumeric + rdefinition["whitespace"] + \
+            ["_", "\""] + rdefinition["close_block"]
+        rdefinition["expr_delim"] = rdefinition["una_op"] + \
+            rdefinition["id_rdef"] + ["!", ","]
+        rdefinition["operator"] = rdefinition["arith_op"] + \
+            rdefinition["rela_op"] + \
+            rdefinition["comb_op"] + rdefinition["una_op"]
+        rdefinition["num_delim"] = (rdefinition["whitespace"] + rdefinition["open_func"] + rdefinition["operator"]
+                                    + rdefinition["close_block"] + rdefinition["separator"] + rdefinition["line_delim"])
+        rdefinition["id_delim"] = rdefinition["num_delim"] + \
+            rdefinition["open_array"]
+        rdefinition["open_codeBlock"] += rdefinition["whitespace"]
+        rdefinition["open_array"] += rdefinition["whitespace"]
+        rdefinition["open_func"] += rdefinition["whitespace"]
 
-    rdefinition["separator"]+=rdefinition["whitespace"]
+        rdefinition["separator"] += rdefinition["whitespace"]
 
-    self.type = []
-    self.value = []
-    self.line = []
-    self.column = []
-    self.error = []
+        self.type = []
+        self.value = []
+        self.line = []
+        self.column = []
+        self.error = []
 
-    for i in range(len(tmpvalue)):
-      if i+1 == len(tmpvalue):
-        self.type.append(tmptype[i])
-        self.value.append(tmpvalue[i])
-        self.error.append(tmperror[i])        
-        break
-      if ((tmpvalue[i] in rdefinition["whitespace"]) or tmptype == "newline"):
-        continue
-      if tmptype[i] in rwords or tmptype[i] in rsymbol or tmptype[i] == "comment":
-        if tmpvalue[i] == 'fs':
-          if tmptype[i+1] == "str_literal":
-            self.type.append(tmptype[i])
-            self.value.append(tmpvalue[i])
-            self.error.append(tmperror[i])            
-          else:
-            self.type.append('lex-error')
-            self.value.append(tmpvalue[i])          
-            self.error.append(f'Lexical Error on Ln {tmpline[i]}, Col {tmpcolumn[i]}: "{tmpvalue[i+1]}" is not a valid delimiter for {tmptype[i]} ')
-            break
-        else:
-          for j,k in deli_reserved.items():
-            if tmpvalue[i] in k:
-              if str(tmpvalue[i+1]) in rdefinition[j] or (True if tmptype[i+1] == ("str_literal" or "char_literal") or tmptype[i+1][0:2] == "id" else False):                
+        for i in range(len(tmpvalue)):
+            if i+1 == len(tmpvalue):
                 self.type.append(tmptype[i])
                 self.value.append(tmpvalue[i])
                 self.error.append(tmperror[i])
-              else:
-                self.type.append('lex-error')
-                self.value.append(tmpvalue[i])          
-                self.error.append(f'Lexical Error on Ln {tmpline[i]}, Col {tmpcolumn[i]}: "{tmpvalue[i+1]}" is not a valid delimiter for {tmptype[i]} ')
                 break
-            else:       
-              continue  
-      elif tmptype[i][0:2] == "id":
-        if tmpvalue[i+1] in rdefinition["id_delim"]:
-          self.type.append(tmptype[i])
-          self.value.append(tmpvalue[i])
-          self.error.append(tmperror[i])
-        else:
-          self.type.append('lex-error')
-          self.value.append(tmpvalue[i])          
-          self.error.append(f'Lexical Error on Ln {tmpline[i]}, Col {tmpcolumn[i]}: "{tmpvalue[i+1]}" is not a valid delimiter for {tmptype[i]} ')
-          break
-      elif tmptype[i] == "str_literal" or tmptype[i] == "char_literal":
-        if tmpvalue[i+1] in rdefinition["id_delim"]:
-          self.type.append(tmptype[i])
-          self.value.append(tmpvalue[i])
-          self.error.append(tmperror[i])
-        else:
-          self.type.append('lex-error')
-          self.value.append(tmpvalue[i])          
-          self.error.append(f'Lexical Error on Ln {tmpline[i]}, Col {tmpcolumn[i]}: "{tmpvalue[i+1]}" is not a valid delimiter for {tmptype[i]} ')
-          break
-      elif tmptype[i] == "int_literal" or tmptype[i] == "deci_literal" or tmptype[i] == "neg_int_literal" or tmptype[i] == "neg_deci_literal":
-        if tmpvalue[i+1] in rdefinition["id_delim"]:
-          self.type.append(tmptype[i])
-          self.value.append(tmpvalue[i])
-          self.error.append(tmperror[i])
-        else:
-          self.type.append('lex-error')
-          self.value.append(tmpvalue[i])          
-          self.error.append(f'Lexical Error on Ln {tmpline[i]}, Col {tmpcolumn[i]}: "{tmpvalue[i+1]}" is not a valid delimiter for {tmptype[i]} ')
-          break
-      else:
-        self.type.append(tmptype[i])
-        self.value.append(tmpvalue[i])
-        self.error.append(tmperror[i])
-        continue
-
-
-
+            if ((tmpvalue[i] in rdefinition["whitespace"]) or tmptype == "newline"):
+                continue
+            if tmptype[i] in rwords or tmptype[i] in rsymbol or tmptype[i] == "comment":
+                if tmpvalue[i] == 'fs':
+                    if tmptype[i+1] == "str_literal":
+                        self.type.append(tmptype[i])
+                        self.value.append(tmpvalue[i])
+                        self.error.append(tmperror[i])
+                    else:
+                        self.type.append('lex-error')
+                        self.value.append(tmpvalue[i])
+                        self.error.append(
+                            f'Lexical Error on Ln {tmpline[i]}, Col {tmpcolumn[i]}: "{tmpvalue[i+1]}" is not a valid delimiter for {tmptype[i]} ')
+                        break
+                else:
+                    for j, k in deli_reserved.items():
+                        if tmpvalue[i] in k:
+                            if str(tmpvalue[i+1]) in rdefinition[j] or (True if tmptype[i+1] == ("str_literal" or "char_literal") or tmptype[i+1][0:2] == "id" else False):
+                                self.type.append(tmptype[i])
+                                self.value.append(tmpvalue[i])
+                                self.error.append(tmperror[i])
+                            else:
+                                self.type.append('lex-error')
+                                self.value.append(tmpvalue[i])
+                                self.error.append(
+                                    f'Lexical Error on Ln {tmpline[i]}, Col {tmpcolumn[i]}: "{tmpvalue[i+1]}" is not a valid delimiter for {tmptype[i]} ')
+                                break
+                        else:
+                            continue
+            elif tmptype[i][0:2] == "id":
+                if tmpvalue[i+1] in rdefinition["id_delim"]:
+                    self.type.append(tmptype[i])
+                    self.value.append(tmpvalue[i])
+                    self.error.append(tmperror[i])
+                else:
+                    self.type.append('lex-error')
+                    self.value.append(tmpvalue[i])
+                    self.error.append(
+                        f'Lexical Error on Ln {tmpline[i]}, Col {tmpcolumn[i]}: "{tmpvalue[i+1]}" is not a valid delimiter for {tmptype[i]} ')
+                    break
+            elif tmptype[i] == "str_literal" or tmptype[i] == "char_literal":
+                if tmpvalue[i+1] in rdefinition["id_delim"]:
+                    self.type.append(tmptype[i])
+                    self.value.append(tmpvalue[i])
+                    self.error.append(tmperror[i])
+                else:
+                    self.type.append('lex-error')
+                    self.value.append(tmpvalue[i])
+                    self.error.append(
+                        f'Lexical Error on Ln {tmpline[i]}, Col {tmpcolumn[i]}: "{tmpvalue[i+1]}" is not a valid delimiter for {tmptype[i]} ')
+                    break
+            elif tmptype[i] == "int_literal" or tmptype[i] == "deci_literal" or tmptype[i] == "neg_int_literal" or tmptype[i] == "neg_deci_literal":
+                if tmpvalue[i+1] in rdefinition["id_delim"]:
+                    self.type.append(tmptype[i])
+                    self.value.append(tmpvalue[i])
+                    self.error.append(tmperror[i])
+                else:
+                    self.type.append('lex-error')
+                    self.value.append(tmpvalue[i])
+                    self.error.append(
+                        f'Lexical Error on Ln {tmpline[i]}, Col {tmpcolumn[i]}: "{tmpvalue[i+1]}" is not a valid delimiter for {tmptype[i]} ')
+                    break
+            else:
+                self.type.append(tmptype[i])
+                self.value.append(tmpvalue[i])
+                self.error.append(tmperror[i])
+                continue
--- app/window.py
+++ app/window.py
@@ -6,23 +6,26 @@ from tkinter import Canvas
 from tkinter import Button
 from tkinter import constants
 
+
 def run_lex():                                  # Run Lexical Analyzer
-    inputValue = inputPane.get("1.0","end-1c")
+    inputValue = inputPane.get("1.0", "end-1c")
     lex = compiler.lexer(inputValue)
     print_lex(lex.type, lex.value)
     print_error(lex.error)
     lexPane.config(state="disabled")
     errorPane.config(state="disabled")
 
-def print_lex(type,value):                      # Print Text to Lexical Pane
+
+def print_lex(type, value):                      # Print Text to Lexical Pane
     lexPane.config(state="normal")
     lexPane.delete('1.0', constants.END)
-    lexPane.insert(constants.END,"LEXEME\t\t\t\tTOKEN\n\n")
+    lexPane.insert(constants.END, "LEXEME\t\t\t\tTOKEN\n\n")
     for i in range(len(type)):
         if type[i] == 'lex-error' or type[i] == 'newline' or type[i] == 'whitespace':
             continue
         else:
-            lexPane.insert(constants.END,f'{str(value[i]) if len(str(value[i]))<=15 else str(value[i])[:10] + "..."}\t\t\t\t{str(type[i])}\n')
+            lexPane.insert(
+                constants.END, f'{str(value[i]) if len(str(value[i]))<=15 else str(value[i])[:10] + "..."}\t\t\t\t{str(type[i])}\n')
             # lexPane.insert(constants.END,f'{"" if type(value[i]) == int else ("" if True else "") }'
             #                              f'\t\t\t\t{str(type[i])}\n')
 
@@ -33,13 +36,14 @@ def print_error(error):                      # Print Text to Error Pane
     for err in range(len(error)):
         if err+1 == len(error):
             if error[err] != '':
-                errorPane.insert(constants.END,f'{error[err]}\n')
+                errorPane.insert(constants.END, f'{error[err]}\n')
         else:
             if error[err] != '':
                 if error[err] != error[err+1]:
-                    errorPane.insert(constants.END,f'{error[err]}\n')
+                    errorPane.insert(constants.END, f'{error[err]}\n')
             else:
                 continue
+
 
 def refresh():
     inputPane.config(state="normal")
@@ -51,127 +55,130 @@ def refresh():
     lexPane.config(state="disabled")
     errorPane.config(state="disabled")
 
+
 window = Tk()
 
 window.geometry("939x617")
-window.configure(bg = "#251c3b")
+window.configure(bg="#251c3b")
 canvas = Canvas(
     window,
-    bg = "#251c3b",
-    height = 617,
-    width = 939,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge")
-canvas.place(x = 0, y = 0)
+    bg="#251c3b",
+    height=617,
+    width=939,
+    bd=0,
+    highlightthickness=0,
+    relief="ridge")
+canvas.place(x=0, y=0)
 
-inputPane_img = PhotoImage(file = f"img/img_textBox0.png")
+inputPane_img = PhotoImage(file=f"img/img_textBox0.png")
 inputPane_bg = canvas.create_image(
     291.5, 272.0,
-    image = inputPane_img)
+    image=inputPane_img)
 
 inputPane = Text(
-    bd = 0,
-    bg = "#2a2247",
-    highlightthickness = 0,
-    fg = "white",
-    padx = 10,
-    pady = 10,
-    font = ("Consolas",10),)
+    bd=0,
+    bg="#2a2247",
+    highlightthickness=0,
+    fg="white",
+    padx=10,
+    pady=10,
+    font=("Consolas", 10),)
 
 inputPane.place(
-    x = 26, y = 110,
-    width = 531,
-    height = 322)
+    x=26, y=110,
+    width=531,
+    height=322)
 
-lexPane_img = PhotoImage(file = f"img/img_textBox1.png")
+lexPane_img = PhotoImage(file=f"img/img_textBox1.png")
 lexPane_bg = canvas.create_image(
     740.5, 353.5,
-    image = lexPane_img)
+    image=lexPane_img)
 
 lexPane = Text(
-    bd = 0,
-    bg = "#2a2247",
-    highlightthickness = 0,
-    fg = "white",
-    padx = 10,
-    pady = 10,
-    font = ("Consolas",10),
+    bd=0,
+    bg="#2a2247",
+    highlightthickness=0,
+    fg="white",
+    padx=10,
+    pady=10,
+    font=("Consolas", 10),
     state="disabled",)
 
 lexPane.place(
-    x = 568, y = 110,
-    width = 345,
-    height = 485)
+    x=568, y=110,
+    width=345,
+    height=485)
 
-errorPane_img = PhotoImage(file = f"img/img_textBox2.png")
+errorPane_img = PhotoImage(file=f"img/img_textBox2.png")
 errorPane_bg = canvas.create_image(
     291.5, 521.0,
-    image = errorPane_img)
+    image=errorPane_img)
 
 errorPane = Text(
-    bd = 0,
-    bg = "#2a2247",
-    highlightthickness = 0,
-    fg = "white",
-    padx = 10,
-    pady = 10,
-    font = ("Consolas",10),
+    bd=0,
+    bg="#2a2247",
+    highlightthickness=0,
+    fg="white",
+    padx=10,
+    pady=10,
+    font=("Consolas", 10),
     state="disabled",)
 
 errorPane.place(
-    x = 26, y = 445,
-    width = 531,
-    height = 150)
+    x=26, y=445,
+    width=531,
+    height=150)
 
-background_img = PhotoImage(file = f"img/background.png")
+background_img = PhotoImage(file=f"img/background.png")
 background = canvas.create_image(
     469.5, 48.5,
     image=background_img)
 
-img0 = PhotoImage(file = f"img/img0.png")            # Run Lexical Analyzer Button
+# Run Lexical Analyzer Button
+img0 = PhotoImage(file=f"img/img0.png")
 lexButton = Button(
-    image = img0,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = run_lex,
-    relief = "flat",
-    background= "#211b36",
-    activebackground = "#211b36")
+    image=img0,
+    borderwidth=0,
+    highlightthickness=0,
+    command=run_lex,
+    relief="flat",
+    background="#211b36",
+    activebackground="#211b36")
 
 lexButton.place(
-    x = 13, y = 52,
-    width = 180,
-    height = 30)
+    x=13, y=52,
+    width=180,
+    height=30)
 
-img1 = PhotoImage(file = f"img/img1.png")            # Run Semantic Analyzer Button
+# Run Semantic Analyzer Button
+img1 = PhotoImage(file=f"img/img1.png")
 semButton = Button(
-    image = img1,
-    borderwidth = 0,
-    highlightthickness = 0,
-    relief = "flat",
-    background= "#211b36",
-    activebackground = "#211b36")
+    image=img1,
+    borderwidth=0,
+    highlightthickness=0,
+    relief="flat",
+    background="#211b36",
+    activebackground="#211b36")
 
 semButton.place(
-    x = 200, y = 52,
-    width = 200,
-    height = 30)
+    x=200, y=52,
+    width=200,
+    height=30)
 
-img2 = PhotoImage(file = f"img/img2.png")            # Bind Button
+img2 = PhotoImage(file=f"img/img2.png")            # Bind Button
 b2 = Button(
-    image = img2,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = refresh,
-    relief = "flat",
-    background = "#099FD1",
-    activebackground = "#099FD1")
+    image=img2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=refresh,
+    relief="flat",
+    background="#099FD1",
+    activebackground="#099FD1")
 
 b2.place(
-    x = 20, y = 6,
-    width = 100,
-    height = 33)
+    x=20, y=6,
+    width=100,
+    height=33)
 
 window.resizable(False, False)
 window.title("Bind Compiler")
